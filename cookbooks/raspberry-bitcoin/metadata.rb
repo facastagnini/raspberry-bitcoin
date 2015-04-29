943f73b1fbf5d4790159b63067b6432c9b9dec4c
Federico Castagnini <federico.castagnini@gmail.com>
2015-04-29 17:15:59 -0400

    updating metadata

--- cookbooks/raspberry-bitcoin/metadata.rb
+++ cookbooks/raspberry-bitcoin/metadata.rb
@@ -1,7 +1,11 @@
 name             'raspberry-bitcoin'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures raspberry-bitcoin'
-long_description 'Installs/Configures raspberry-bitcoin'
+maintainer       'Federico Castagnini'
+maintainer_email 'federico.castagnini@gmail.com'
+license          'Apache v2.0'
+description      'Installs/Configures bitcoin core on a raspberry pi B 2'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+issues_url       'https://github.com/facastagnini/raspberry-bitcoin/issues'
+source_url       'https://github.com/facastagnini/raspberry-bitcoin'
+
+depends 'raspberry-pi'
