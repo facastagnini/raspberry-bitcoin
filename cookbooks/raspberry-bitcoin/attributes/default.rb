871f7812e1f0ad5708018be642d3dd786183aa31
Federico Castagnini <federico.castagnini@gmail.com>
2015-04-30 20:56:38 -0400

    pinning the version of bitcoin core

--- cookbooks/raspberry-bitcoin/attributes/default.rb
+++ cookbooks/raspberry-bitcoin/attributes/default.rb
@@ -3,3 +3,6 @@ default['apt']['unattended_upgrades']['auto_fix_interrupted_dpkg'] = true
 default['apt']['unattended_upgrades']['allowed_origins'] = [
   "#{node['platform'].capitalize} stable"
 ]
+
+default['bitcoin']['source']['version'] = 'v0.10.1'
+default['bitcoin']['source']['checksum'] = '18a88785748588bb90764dc7f0aad5548da880d4d91f5803c7076cbcadb2959e'
