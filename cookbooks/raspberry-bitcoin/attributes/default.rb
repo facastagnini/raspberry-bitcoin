39fbea40213aeddf21869f77bfc04caa637c2304
Federico Castagnini <federico.castagnini@gmail.com>
2015-08-18 22:58:52 -0400

    disabling timezone due to a bug in timezone_lwrp
    https://github.com/dragonsmith/timezone_lwrp/pull/1

--- cookbooks/raspberry-bitcoin/attributes/default.rb
+++ cookbooks/raspberry-bitcoin/attributes/default.rb
@@ -8,4 +8,4 @@ default['bitcoin']['source']['url'] = 'https://github.com/bitcoinxt/bitcoinxt'
 default['bitcoin']['source']['version'] = '0.11A'
 default['bitcoin']['source']['checksum'] = 'bd7a9226a14e74aab5ae684e7fa90d7815a2335c'
 
-default['tz'] = 'EDT'
+#default['tz'] = 'EDT'
