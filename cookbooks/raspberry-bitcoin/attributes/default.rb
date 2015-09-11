d7037a3afa12043fbe407de32f9f87802ec16c60
Federico Castagnini <federico.castagnini@gmail.com>
2015-09-11 02:03:39 -0400

    Revert "updating checksum"
    This reverts commit 367a0dde8222b7dd2fb0d8d22a87a70606920bb4.

--- cookbooks/raspberry-bitcoin/attributes/default.rb
+++ cookbooks/raspberry-bitcoin/attributes/default.rb
@@ -7,5 +7,4 @@ default['apt']['unattended_upgrades']['allowed_origins'] = [
 default['bitcoin']['source']['version'] = '0.10.1'
 default['bitcoin']['source']['checksum'] = '18a88785748588bb90764dc7f0aad5548da880d4d91f5803c7076cbcadb2959e'
 
-
 # default['tz'] = 'EDT'
